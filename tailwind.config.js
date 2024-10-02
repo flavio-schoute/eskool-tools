f5b0f142af5624afab883c5a5b04735265a32b83
Flavio Schoute <flavio03outlook.com>
2024-10-02 10:16:34 +0000

    Setup laravel permissions and writedown todo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        
         './app/Http/Livewire/**/*Table.php',
         './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
         './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php'
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -25,6 +23,5 @@ export default {
             },
         },
     },
-
     plugins: [forms],
 };
