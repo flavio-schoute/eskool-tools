0f0d1fe8966d51068272522bdd39e596ac95b0cc
Flavio Schoute <flavio03outlook.com>
2024-10-09 11:14:41 +0000

    wip: sidebar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,27 +1,25 @@
-<nav class="min-h-screen w-full max-w-64 flex border-r-2 border-r-[#e6e7e8] p-4 bg-white ">
+<nav class="min-h-screen w-full max-w-64 flex border-r-2 border-r-[#e6e7e8] p-4 bg-white">
     <div class="w-full">
-        <div>
+        <div class="mb-4">
             <x-application-logo class="block w-auto fill-current h-20" />
         </div>
 
-        <div class="p-4">
+        <div class="mb-4">
             <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
         </div>
 
-        <div>
-            <ul class="flex flex-col gap-3">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-nav-link>
+        <div class="flex flex-col gap-3">
+            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-nav-link>
 
-                <x-nav-link :href="route('sales-overview.index')" :active="request()->routeIs('sales-overview.index')">
-                    {{ __('Sales overzicht') }}
-                </x-nav-link>
+            <x-nav-link :href="route('sales-overview.index')" :active="request()->routeIs('sales-overview.index')">
+                {{ __('Sales overzicht') }}
+            </x-nav-link>
 
-                <x-nav-link :href="route('debtor-management.index')" :active="request()->routeIs('debtor-management.index')">
-                    {{ __('Debiteuren overzicht') }}
-                </x-nav-link>
-            </ul>
+            <x-nav-link :href="route('debtor-management.index')" :active="request()->routeIs('debtor-management.index')">
+                {{ __('Debiteuren overzicht') }}
+            </x-nav-link>
         </div>
     </div>
 </nav>
